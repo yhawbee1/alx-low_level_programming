829ca125f1faf74105c3dc0edfa73d623e81dc48
yhawbee1 <antwibrian82@gmail.com>
2023-03-17 10:14:00 -0700

    Alphabet printer

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main() {
+	char c = 'a';
+    while (c <= 'z') {
+	    putchar(c);
+	c++;
+    }
+    putchar('\n');
+    return 0;
+}
