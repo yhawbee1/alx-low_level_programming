69033d058eb6d621f3c4b4e60000e2907c65f791
yhawbee1 <antwibrian82@gmail.com>
2023-03-20 03:03:55 -0700

    Base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*/ 
+int main(void)
+{
+	int i;
+	char c;
+	
+	for (i = 0; i < 10; i++)
+	{
+		c = i + '0';
+		putchar(c);
+	}
+	for (i = 10; i < 16; i++)
+	{
+		c = i - 10 + 'a';
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
+
