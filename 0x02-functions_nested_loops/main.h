cc177c5e576791888849a7e7d25507164b73770f
yhawbee1 <antwibrian82@gmail.com>
2023-03-22 09:44:28 -0700

    Functions and nested loops.

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#ifndef main_h
-#define main_h
-
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -13,5 +10,3 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_tables(int n);
-
-#endif
